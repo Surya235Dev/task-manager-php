5ad4899b8f6a5c3f5e15519113d11ce4af6f0074
Surya235Dev <37703729+Surya235Dev@users.noreply.github.com>
2020-05-31 11:28:23 +0530

    Add files via upload

---
+++ components/nav1.php
@@ -0,0 +1,4 @@
+<nav class="navbar navbar-light bg-light justify-content-between">
+  <a class="navbar-brand">Task Organiser</a>
+   <a href="index.php" class="btn btn-outline-success my-2 my-sm-0" type="submit">View all Task</a>
+</nav>
